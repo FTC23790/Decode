34f3ada6977aeb269d546b7346641fdc043dba9b
FTC23790 <ridetherobot8559@gmail.com>
2025-10-09 19:19:27 -0400

    removed lfit and scoring system fronm vision tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Clanker_Tele_Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/Clanker_Tele_Vision.java
@@ -8,8 +8,6 @@ import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Decode.Setup_Subfiles.Driving_System;
-//import org.firstinspires.ftc.teamcode.Decode.Setup_Subfiles.Lifting_System;
-import org.firstinspires.ftc.teamcode.Decode.Setup_Subfiles.Scoring_System;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
@@ -17,10 +15,6 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 public class Clanker_Tele_Vision extends OpMode {
     Driving_System DS = new Driving_System();
     // drive system object
-    //Lifting_System LS = new Lifting_System();
-    //lifting system object
-    Scoring_System SS = new Scoring_System();
-    // Intake/outtake object
     double LX1;
     double LY1;
     double RX1;
@@ -31,8 +25,6 @@ public class Clanker_Tele_Vision extends OpMode {
     IMU imu;
     double PowerMod;
     boolean A1;
-    boolean X2;
-    boolean B2;
 
     // Limelight declaration
     private Limelight3A limelight = null;
@@ -48,10 +40,7 @@ public class Clanker_Tele_Vision extends OpMode {
         imu.initialize(new IMU.Parameters(RevOrientation));
 
         DS.Drive_MotorCal(hardwareMap);
-        //LS.Lift_MotorCal(hardwareMap);
-        SS.Score_MotorCal(hardwareMap);
         imu.resetYaw();
-        //LS.RSTarget(-25);
         //Initilise HardwareMap setup
 
     try {
@@ -73,8 +62,6 @@ public class Clanker_Tele_Vision extends OpMode {
         TL1 = gamepad1.left_trigger;
         TR1 = gamepad1.right_trigger;
         A1 = gamepad1.a;
-        X2 = gamepad2.x;
-        B2 = gamepad2.b;
         LB2 = gamepad2.left_bumper;
         RB2 = gamepad2.right_bumper;
         // gamepad setting
@@ -95,14 +82,6 @@ public class Clanker_Tele_Vision extends OpMode {
         DS.Drive_Running();
         // drive system module
 
-        //LS.Lift_Grabber(LB2, RB2);
-        //LS.RightLiftSys();
-        // lift system module
-
-        SS.Scoring_Grabber(X2, B2);
-        SS.Scoring_Running();
-        // Scoring system module
-
         telemetry.addData("Heading", ( DS.RAPrint() / ( 2 * 3.14159 ) ) * 360 );
         telemetry.addData("Power", PowerMod );
         telemetry.addData("X Input",LX1);
@@ -115,8 +94,8 @@ public class Clanker_Tele_Vision extends OpMode {
 
 
 
-    @Override
-    public void stop() {
+   // @Override
+//public void stop() {
         // Stop Limelight polling when opmode stop
         if (limelight != null) {
             limelight.stop();
