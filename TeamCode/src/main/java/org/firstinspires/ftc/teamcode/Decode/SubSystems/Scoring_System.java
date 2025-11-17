bb6b986fd53dff4fdb649f9bc5b025d3141c6879
FTC23790 <ridetherobot8559@gmail.com>
2025-11-17 18:05:02 -0500

    changed the scoring system to use the intake as a servo and not a motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/SubSystems/Scoring_System.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/SubSystems/Scoring_System.java
@@ -5,24 +5,26 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Decode.Setup.Motor_Servo_Setup.Drive_Motor_Setup;
-import org.firstinspires.ftc.teamcode.Decode.Setup.Motor_Servo_Setup.Servo_Setup;
+import org.firstinspires.ftc.teamcode.Decode.Setup.Motor_Servo_Setup.Cont_Servo_Setup;
 public class Scoring_System {
-    Drive_Motor_Setup CH2IN = new Drive_Motor_Setup();
+    Cont_Servo_Setup CH0IN = new Cont_Servo_Setup();
+    // change object name baseded on port
     Drive_Motor_Setup EH2OUT = new Drive_Motor_Setup();
-    Servo_Setup EH1S = new Servo_Setup();
-    Servo_Setup CH0S = new Servo_Setup();
+    //Cont_Servo_Setup EH1S = new Cont_Servo_Setup();
+    //Cont_Servo_Setup CH0S = new Cont_Servo_Setup();
     private double InTake;
     private boolean OutTake_Button;
     private double PassThrough;
     private double Outtake_RPM;
     public void Score_MotorCal (HardwareMap hardwareMap) {
-        CH2IN.init(hardwareMap, "CH2Intake");
+        CH0IN.init(hardwareMap, "CH0IntakeServo");
         EH2OUT.init(hardwareMap, "EH2OutTake");
-        EH1S.init(hardwareMap, "RBP");
-        CH0S.init(hardwareMap, "LBP");
-        CH2IN.setDirection(DcMotorSimple.Direction.FORWARD);
+        //EH1S.init(hardwareMap, "RBP");
+        //CH0S.init(hardwareMap, "LBP");
+        CH0IN.setDirection(DcMotorSimple.Direction.FORWARD);
+        //unsure on direation of servo
         EH2OUT.setDirection(DcMotorSimple.Direction.REVERSE);
-        EH1S.setDirection(DcMotorSimple.Direction.FORWARD);
+        //EH1S.setDirection(DcMotorSimple.Direction.FORWARD);
         // setup
     }
 
@@ -39,9 +41,9 @@ public class Scoring_System {
         } else {
             EH2OUT.setMotorSpeed(0);
         }
-        CH2IN.setMotorSpeed(InTake);
-        EH1S.setPower(PassThrough);
-        CH0S.setPower(PassThrough);
+        CH0IN.setPower(InTake);
+        //EH1S.setPower(PassThrough);
+        //CH0S.setPower(PassThrough);
     }
     public int Out_Return () {
         return EH2OUT.CurrentPos();
